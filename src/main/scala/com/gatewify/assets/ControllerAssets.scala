e74ccbf37f3897e67e093c8653090a8b06e9423b
git_skwr <tiago.dias@skullwritter.com>
2019-07-16 10:19:56 +0000

    Code Update

--- src/main/scala/com/gatewify/assets/ControllerAssets.scala
+++ src/main/scala/com/gatewify/assets/ControllerAssets.scala
@@ -1,12 +1,23 @@
 package com.gatewify.assets;
 
-import gitbucket.core.api._
-import gitbucket.core.controller._
 import gitbucket.core.controller.api._
 import gitbucket.core.controller.ControllerBase
 import gitbucket.core.controller.RepositoryViewerController
 
-class ControllerAssets extends ControllerBase {
+import java.io.{ByteArrayInputStream, File}
+import java.io.File
+
+import gitbucket.core.api._
+import gitbucket.core.controller.ControllerBase
+import gitbucket.core.service.{AccountService, ReleaseService}
+import gitbucket.core.util.Directory.getReleaseFilesDir
+import gitbucket.core.util.{FileUtil, ReferrerAuthenticator, RepositoryName, WritableUsersAuthenticator}
+import gitbucket.core.util.Implicits._
+import gitbucket.core.util.SyntaxSugars.defining
+import org.apache.commons.io.FileUtils
+import org.scalatra.{Created, NoContent}
+
+class ControllerAssets extends ControllerBase implements ApiReleaseControllerBase, ReleaseControllerBase{
 	get("/api/v3/repos/:owner/:repository/releases/:tag/assets/latest")(referrersOnly { repository =>
 		val name = params("tag")
 		getReleaseAsset(repository.owner, repository.name, name, fileId)
