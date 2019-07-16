8723211eb2df434a03c39922c334693945098cc9
git_skwr <tiago.dias@skullwritter.com>
2019-07-16 10:23:27 +0000

    Code Update

--- src/main/scala/com/gatewify/assets/ControllerAssets.scala
+++ src/main/scala/com/gatewify/assets/ControllerAssets.scala
@@ -17,7 +17,7 @@ import gitbucket.core.util.SyntaxSugars.defining
 import org.apache.commons.io.FileUtils
 import org.scalatra.{Created, NoContent}
 
-class ControllerAssets extends ControllerBase implements ApiReleaseControllerBase, ReleaseControllerBase{
+class ControllerAssets extends ControllerBase with ApiReleaseControllerBase with ReleaseControllerBase{
 	get("/api/v3/repos/:owner/:repository/releases/:tag/assets/latest")(referrersOnly { repository =>
 		val name = params("tag")
 		getReleaseAsset(repository.owner, repository.name, name, fileId)
